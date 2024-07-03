6f9bfea317bd5b353f44dd44fa13ae49a50c82b0
menezestrigueiros <156216447+menezestrigueiros@users.noreply.github.com>
2024-07-03 13:42:31 +0200

    Update WWAngleCalculations.h

--- WWAngleCalculations.h
+++ WWAngleCalculations.h
@@ -21,20 +21,23 @@ using namespace WWTools;
 
 
 
-/** Processor calculates decay angles from W boson.
+/** Processor calculates MC decay angles from W boson.
  *  PROTOCOL
  * 1) Goes through MC Particles
  * 
- * 2) ON SHELL W has PDG abs(24) -> production angle of leptonic W 
- *  2.1) Discrimination for LEPTONIC and HADRONIC decay -> GetDaughters() -> GetPDG 
- *  2.2) If this a lepton PDG, this is a leptonic W -> production angle of leptonic Ws
- *  2.3) Use starVector -> *theta and *phi
+ * 2) [ON SHELL] W has PDG abs(24) ---> Get four-momentum && Charge
+ *  -  Production angle of W- (CosineTheta)
+ *  -  LEPTONIC and HADRONIC decay discrimination (Directly from W daughters).
+ *  -  LEPTONIC DECAY FROM ---> W- or W+ (cross check by counting) ---> For decay angles
+ *  -  HADRONIC DECAY FROM ---> W- or W+ (cross check by counting)
+ *  -  DECAY ANGLES ---> Use starVector ---> *theta and *phi
  * 
- * 3) OFF SHELL W reconstructed from it's daughters:
- *  3.1) Discrimination for LEPTONIC and HADRONIC decay (pdg > or < than 10) -> production angle of leptonic Ws
- *  3.2) LEPTONIC FROM ---> W- or W+ (cross check by counting)
- *  3.3) HADRONIC FROM ---> W- or W+ (cross check by counting)
- *  3.4) Use starVector -> *theta and *phi
+ * 3) [OFF SHELL] W reconstructed from it's daughters ---> Get four-momentum && Charge
+ *  -  Production angle of W- (CosineTheta)
+ *  -  LEPTONIC and HADRONIC decay discrimination (pdg > or < than 10) 
+ *  -  LEPTONIC FROM ---> W- or W+ (cross check by counting) ---> Use for decay angles
+ *  -  HADRONIC FROM ---> W- or W+ (cross check by counting)
+ *  -  DECAY ANGLES ---> Use starVector -> *theta and *phi
  * 
  *  @author A.Silva, DESY
  */
